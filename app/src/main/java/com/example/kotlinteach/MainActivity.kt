61a1d6367a0d5a7f9ff4692eccb2a7b717b43dfb
Peter Uhlimov <businessmail1710@mail.ru>
2021-12-10 22:01:36 +0400

    Initial commit

---
+++ app/src/main/java/com/example/kotlinteach/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.kotlinteach
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.delay
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        process()
+
+    }
+    private suspend fun process(){
+        for (i in 1..5){
+            delay(500L)
+            Log.i("MyLog","HELLO : $i")
+        }
+        Log.i("MyLog","Process was ready")
+    }
+
+
+
+}
